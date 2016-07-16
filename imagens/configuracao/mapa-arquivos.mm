0bbfbeef5543da78c4de032fbd805617f13c8f8b
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2016-07-16 17:59:02 -0300

    Estrutura dos arquivos

---
+++ imagens/configuracao/mapa-arquivos.mm
@@ -0,0 +1,113 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Arquivos" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1468696605816"><hook NAME="MapStyle" zoom="1.5">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<node TEXT="Entrada" POSITION="left" ID="ID_782159025" CREATED="1468697070944" MODIFIED="1468697073104">
+<edge COLOR="#007c7c"/>
+<node TEXT="configuracao.pdf" ID="ID_1830077850" CREATED="1468696591631" MODIFIED="1468697407960">
+<font BOLD="true"/>
+<node TEXT="Capa" ID="ID_18603044" CREATED="1468696933724" MODIFIED="1468696935691"/>
+<node TEXT="Folha de rosto" ID="ID_5734928" CREATED="1468696936860" MODIFIED="1468696940542"/>
+<node TEXT="Folha de Aprova&#xe7;&#xe3;o" ID="ID_1939326370" CREATED="1468696941141" MODIFIED="1468697167491"/>
+<node TEXT="Dedicat&#xf3;ria" ID="ID_771842181" CREATED="1468697318140" MODIFIED="1468697323091"/>
+<node TEXT="Agradecimentos" ID="ID_1058177122" CREATED="1468697323836" MODIFIED="1468697327808"/>
+<node TEXT="Ep&#xed;grafe" ID="ID_152745102" CREATED="1468697338574" MODIFIED="1468697343782"/>
+<node TEXT="Resumo" ID="ID_1848025353" CREATED="1468697344401" MODIFIED="1468697348540"/>
+<node TEXT="Abstract" ID="ID_1660888005" CREATED="1468697349045" MODIFIED="1468697352718"/>
+<node TEXT="Lista de ilustra&#xe7;&#xf5;es" ID="ID_1832555492" CREATED="1468697367337" MODIFIED="1468697374218"/>
+<node TEXT="Lista de tabelas" ID="ID_1717697796" CREATED="1468697374828" MODIFIED="1468697379709"/>
+<node TEXT="Lista de siglas" ID="ID_533213060" CREATED="1468697380446" MODIFIED="1468697384477"/>
+<node TEXT="Lista de s&#xed;mbolos" ID="ID_836966605" CREATED="1468697385019" MODIFIED="1468700383831"/>
+</node>
+<node TEXT="trabalho-academico.md" ID="ID_1944772862" CREATED="1468696635929" MODIFIED="1468700244324">
+<font BOLD="true"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Onde voc&#xea; ir&#xe1; escrever&#xa;o texto do seu trabalho" ID="ID_967917131" CREATED="1468696644477" MODIFIED="1468700347412"/>
+</node>
+<node TEXT="referencias.md" ID="ID_945933372" CREATED="1468696725691" MODIFIED="1468697410450">
+<font BOLD="true"/>
+<node TEXT="As ref&#xea;ncias bibliogr&#xe1;ficas" ID="ID_1676402682" CREATED="1468696735111" MODIFIED="1468697573591"/>
+</node>
+<node TEXT="apendices.md" ID="ID_938379587" CREATED="1468696779549" MODIFIED="1468697411335">
+<font BOLD="true"/>
+<node TEXT="Texto dos ap&#xea;ndices." ID="ID_1228627033" CREATED="1468696792842" MODIFIED="1468696823431"/>
+</node>
+<node TEXT="anexos.md" ID="ID_468154768" CREATED="1468696803730" MODIFIED="1468697412158">
+<font BOLD="true"/>
+<node TEXT="Texto dos anexos." ID="ID_231195194" CREATED="1468696811821" MODIFIED="1468696817357"/>
+</node>
+<node TEXT="imagens/ficha-catalografica.pdf" ID="ID_368082840" CREATED="1468696975067" MODIFIED="1468697414719">
+<font BOLD="true"/>
+</node>
+<node TEXT="imagens/folha-de-aprovacao-escaneada.pdf" ID="ID_1073127623" CREATED="1468697460924" MODIFIED="1468697468935">
+<font BOLD="true"/>
+</node>
+<node TEXT="errata.md" ID="ID_185021965" CREATED="1468696618463" MODIFIED="1468697581652">
+<font BOLD="true"/>
+<node TEXT="Errata" ID="ID_227298647" CREATED="1468696711683" MODIFIED="1468696722943"/>
+</node>
+</node>
+<node TEXT="Sa&#xed;da" POSITION="right" ID="ID_1299268828" CREATED="1468697051905" MODIFIED="1468697055298">
+<edge COLOR="#7c007c"/>
+<node TEXT="xxx-Monografia.pdf" ID="ID_203586884" CREATED="1468696894390" MODIFIED="1468697529021">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="xxx-Disserta&#xe7;&#xe3;o.pdf" ID="ID_559776067" CREATED="1468696901733" MODIFIED="1468696915814"/>
+<node TEXT="xxx-Tese.pdf" ID="ID_1502916735" CREATED="1468696903897" MODIFIED="1468696923194"/>
+<node TEXT="Depende do tipo&#xa;do seu trabalho." ID="ID_1026821917" CREATED="1468697529010" MODIFIED="1468697549422">
+<hook NAME="SummaryNode"/>
+</node>
+<node TEXT="xxx-*" ID="ID_1399500916" CREATED="1468696850599" MODIFIED="1468697067897">
+<node TEXT="Arquivos tempor&#xe1;rios" ID="ID_861980833" CREATED="1468696867648" MODIFIED="1468696872519"/>
+</node>
+</node>
+</node>
+</map>
