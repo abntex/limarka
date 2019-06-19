72eeacb70b2e534ea25414638559049545b1eae5
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-06-18 21:31:24 -0300

    Correção do script de deploy #179

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -2,12 +2,12 @@
 #   v1.2.3
 #   dev
 # http://codewiki.wikidot.com/shell-script:if-else
-if [ -n = "$1" ]
+if [ -n "$1" ]
 then
     # Versionamento ANO.MÊS
     # https://semver.org/lang/pt-BR/
     ANO=`echo $1|cut -f 1 -d '.'`
-    MES=`echo $2|cut -f 2 -d '.'`
+    MES=`echo $1|cut -f 2 -d '.'`
     declare -a tags=("limarka/limarka" "limarka/limarka:$ANO" "limarka/limarka:$ANO.$MES")
 else
     declare -a tags=("limarka/limarka:dev")
