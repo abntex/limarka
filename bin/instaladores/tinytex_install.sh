50c27a5456f4e65c34a33388465b164cdc66c4e0
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2020-07-13 19:06:13 -0300

    Alterando mirror do tinytex closes #204

--- bin/instaladores/tinytex_install.sh
+++ bin/instaladores/tinytex_install.sh
@@ -4,8 +4,11 @@ if [ "$IGNORE_CACHE" != "1" ] && command -v tlmgr > /dev/null; then
   tex_em_cache=1
   echo TinyTeX não será instalado pois foi encontrado no cache.
 else
-    wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
-    ~/bin/tlmgr --no-verify-downloads install \
+  # O mirror //linorg.usp.br/CTAN está muito lento, uilizando outro
+    wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | CTAN_REPO=http://mirrors.rit.edu/CTAN/systems/texlive/tlnet sh
+    tlmgr option repository http://mirrors.rit.edu/CTAN/systems/texlive/tlnet
+
+    ~/bin/tlmgr --verify-repo=none install \
       abntex2 \
       babel-portuges \
       biber \
